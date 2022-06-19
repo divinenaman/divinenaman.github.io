d335a883a2f919c36693a398f5c224e9e7ed3e55
Naman Agarwal <naman.agarwal2019@vitstudent.ac.in>
2022-06-19 17:08:41 +0530

    fix caption

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 images = [
     {
         link: "https://unsplash.com/photos/dnAWPVyaUGc/download?ixid=MnwxMjA3fDB8MXxhbGx8Mnx8fHx8fDJ8fDE2NTU2MTM5Mzg&force=true&w=1920",
-        caption: "A sotry but the lack of words"
+        caption: "A story but the lack of words"
     },
     {
         link: "https://unsplash.com/photos/QGvyw8py8xk/download?ixid=MnwxMjA3fDB8MXxhbGx8MXx8fHx8fDJ8fDE2NTU2MTM5Mzg&force=true&w=1920",
