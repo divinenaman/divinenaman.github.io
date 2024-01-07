4c2ec4879198cd806c6e2a375e8e7aa30f50e6ea
Naman Agarwal <namanddev7@gmail.com>
2024-01-08 01:40:40 +0530

    readlist : 2024-01-07

--- js/readlist.js
+++ js/readlist.js
@@ -4,7 +4,8 @@ let blogs = {
 	"2024-01-01": [source("https://simonwillison.net/2023/Dec/31/ai-in-2023/", "Stuff we figured out about AI in 2023")],
 	"2024-01-02": [source("https://bytes.swiggy.com/swiggy-cache-on-delivery-3dced3621dde", "Cache-On-Delivery: Custom caching in webview")],
 	"2024-01-03": [source("https://xeiaso.net/talks/unix-philosophy-logical-extreme-wasm/", "Xe's Blog on Wazero: Go WebAssebly Runtime")],
-        "2024-01-06": [source("https://antonz.org/in-browser-code-playgrounds/", "In browser code playgrounds : using WASI Runtime")]
+        "2024-01-06": [source("https://antonz.org/in-browser-code-playgrounds/", "In browser code playgrounds : using WASI Runtime")],
+	"2024-01-07": [source("https://birtles.blog/2024/01/06/weird-things-engineers-believe-about-development/", "Weird Things Engineers believe about WEB Dev")]
 }
 
 const container = document.querySelector(".section--readlist")
