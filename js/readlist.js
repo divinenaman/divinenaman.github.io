9821bb81a5ae4989e8ad0dc31f5de19a3eef549c
Naman Agarwal <namanddev7@gmail.com>
2024-05-28 23:49:37 +0530

    readlist: 2024-05-28

--- js/readlist.js
+++ js/readlist.js
@@ -39,6 +39,7 @@ let blogs = {
         , "2024-05-02": [source("https://michal.sapka.me/bsd/why-bsd/", "Why use BSD ?")]
 	, "2024-05-03": [source("https://xp123.com/refactor-inline-adjust-extract/", "Refactor: inline-adjust-extract")]
         , "2024-05-06": [source("https://yoric.github.io/post/rust-typestate/", "Rust Typestate"), source("https://transactional.blog/blog/2023-why-full-text-search-is-hard", "Full text search is hard")]
+        , "2024-05-28": [source("https://kyleshevlin.com/two-types-of-composition/", "Two types of composition in frontend!"), source("https://kyleshevlin.com/design-system-retrospective/", "Creating a design system!")]
 }
 
 const container = document.querySelector(".section--readlist")
